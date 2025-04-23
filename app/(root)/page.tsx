5d1f84785c5f213c6464f239a7197d78a1f0ffdb
NKC27 <93439579+NKC27@users.noreply.github.com>
2025-04-23 21:14:16 +0100

    feat: Update layout and components for improved UI/UX
    - Enhanced `layout` for better structure and responsiveness.
    - Updated `page.tsx` to reflect the latest design changes.
    - Improved `InterviewCard` component for a more interactive experience.
    - Modified `index` for optimized loading and organization.
    - Updated `displayTechIcons` for better visual representation of technologies.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,59 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
+import { dummyInterviews } from '@/constants';
+import InterviewCard from '@/components/InterviewCard';
 
-const page = () => {
-  return <div>Homepage</div>;
+const Page = () => {
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview questions and get instant feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Get Started</Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="robot"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+
+      <section className="flex flex-col mt-8 gap-6">
+        <h2>Your Interviews</h2>
+        <div className="interview-section">
+          {dummyInterviews.length > 0 ? (
+            dummyInterviews.map((interview, index) => (
+              <InterviewCard key={`your-${index}`} {...interview} />
+            ))
+          ) : (
+            <p>You have not taken any interviews yet</p>
+          )}
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an interview</h2>
+        <div className="interviews-section">
+          {dummyInterviews.length > 0 ? (
+            dummyInterviews.map((interview, index) => (
+              <InterviewCard key={`take-${index}`} {...interview} />
+            ))
+          ) : (
+            <p>No interviews available</p>
+          )}
+        </div>
+      </section>
+    </>
+  );
 };
 
-export default page;
+export default Page;
