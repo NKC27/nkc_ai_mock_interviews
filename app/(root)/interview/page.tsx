08723051fcd9c02840fca82b65c3f87bbf35183b
NKC27 <93439579+NKC27@users.noreply.github.com>
2025-05-01 13:39:34 +0100

    feat: create Agent component for AI interviewer and update layout title

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Agent from '@/components/Agent';
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <h3 className="">
+        Interview Generator
+        <Agent userName="You" userId="user1" type="generate" />
+      </h3>
+    </>
+  );
 };
 
 export default page;
